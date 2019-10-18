4519924326c237fea7f87ec7fa7bc26a38275669
Potzy12323123125y7 <alexander.naslund2@elev.ga.ntig.se>
2019-10-18 11:14:41 +0200

    Second commit 2019-10-18

--- src/Gissatalet.java
+++ src/Gissatalet.java
@@ -1,9 +1,29 @@
 
 import java.util.*;
 import javax.swing.*;
+import java.text.*;
 public class Gissatalet {
 
     public static void main(String[] args) {
+        Random rand = new Random();
+        int tal = rand.nextInt(100);
+        int a = 0;
+        int gissningar = 0;
 
-}
+        while (a != tal) {
+            a = Integer.parseInt(JOptionPane.showInputDialog(null, "gissa talet?"));
+            gissningar++;
+            if (tal > a)
+                  JOptionPane.showMessageDialog(null, "du gissade för lågt" +a);
+
+            else if (a > tal)
+                    JOptionPane.showMessageDialog(null, "Du gissade för högt" +a);
+            if (a == tal) {
+                JOptionPane.showMessageDialog(null, "Du gissade rätt efter x antal gissnigar x =" +gissningar);
+            }
+        }
+
+
+
+    }
 }
